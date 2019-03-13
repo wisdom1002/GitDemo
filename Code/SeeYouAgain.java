01db1f78b4017082cfdcf42dc42c0acf98e76a57
wisdom1002 <luhuaicai1002@163.com>
2019-03-13 16:47:59 +0800

    Update SeeYouAgain.java

--- Code/SeeYouAgain.java
+++ Code/SeeYouAgain.java
@@ -3,5 +3,19 @@ package com.lhc.study;
 public class TestGit{
 	public static void mainn(String{} arg0){
 		System.out.println("This is first looking for each other , Git is great tools for codeing worker!");
+		MyGitSay xiao = new MyGitSay();
+		xiao.sayHello(1);
+	}
+}
+
+class MyGitSay{
+	public String sayHello(int type){
+		if(type == 1 ){
+			System.out.println(1);
+		}elseif(type > 1){
+			System.out.println(2);
+		}else{
+			System.out.println(3);
+		}
 	}
 }
