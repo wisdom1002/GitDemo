a4ec56db1b622951e5639bc0df54d3e83fcf74f6
luhuaicai <luhuaicai1002@163.com>
2019-03-13 14:48:11 +0800

    分支提交代码

---
+++ Code/SeeYouAgain.java
@@ -0,0 +1,7 @@
+package com.lhc.study;
+
+public class TestGit{
+	public static void mainn(String{} arg0){
+		System.out.println("This is first looking for each other , Git is great tools for codeing worker!");
+	}
+}
