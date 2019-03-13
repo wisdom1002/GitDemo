26663ee1e51f0c255175d0f73218f9a319984253
luhuaicai <luhuaicai1002@163.com>
2019-03-13 16:50:24 +0800

    本地版本库修改SeeYouAgain.java文件

--- Code/SeeYouAgain.java
+++ Code/SeeYouAgain.java
@@ -3,5 +3,6 @@ package com.lhc.study;
 public class TestGit{
 	public static void mainn(String{} arg0){
 		System.out.println("This is first looking for each other , Git is great tools for codeing worker!");
+		System.out.println("This is second looking for each other , Git is great tools for codeing worker!");
 	}
 }
